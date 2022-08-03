f7eabc1cf38ea1b48b49f85a5dfa4475968cd708
NicolasMC94 <mcnicolas.94@gmail.com>
2022-08-03 19:28:47 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let arrayNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let arrayCards = ["♦", "♥", "♠", "♣"];
+
+  let numero = arrayNumbers[Math.floor(Math.random(arrayNumbers.length))];
+  let palo = arrayCards[Math.floor(Math.random(arrayCards.length))];
 };
